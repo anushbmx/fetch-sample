57bbcf3cebee225579fa221db42628f9d178a9d5
Anush A <anush@techstream.org>
2022-02-16 16:07:35 +0530

    Added lodash package

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
+import React, {Component} from 'react';
+import {Navbar, Container} from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends Component {
+
+  componentDidMount = () => {
+    this.fetchInitData();
+  };
+
+  fetchInitData = () => {
+    const baseUrl = 'https://swapi.dev/api';
+    const {page} = this.state;
+    fetch(`${baseUrl}/people/${page}`, {
+        method: 'get'
+      })
+      .then(()=>{
+
+      })
+      .catch(()=>{
+
+      })
+  }
+
+  render = () => {
+    return (
+      <div className='container'>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+          React Bootstrap
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      <div className='row'>
+        <div className='col-12'>
+        <h1>List</h1>
+        </div>
+      </div>
     </div>
-  );
+    )
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      page: 1,
+      loading: true,
+      data: [],
+    };
+  }
+
 }
 
 export default App;
